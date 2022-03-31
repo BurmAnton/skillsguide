9cc42243278022e86c0678a53e330c2b55ce3149
BurmAnton <a.burmatsky@gmail.com>
2022-03-31 16:30:50 +0400

    change admin app on custom

---
+++ skillsguide/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+class MyAdminSite(admin.AdminSite):
+    site_header = 'Центр опережающей профессиональной подготовки (Самарской области)'
+    site_title = 'ЦОПП СО'
