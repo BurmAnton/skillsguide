5d7e882d516bdf284bc76b742354aaf31cebd342
Anton <a.burmatsky@gmail.com>
2022-10-24 15:58:14 +0400

    Add students_count to admin

--- schools/admin.py
+++ schools/admin.py
@@ -19,12 +19,18 @@ class GradeInLine(admin.TabularInline):
 # Register your models here.
 @admin.register(School)
 class SchoolAdmin(admin.ModelAdmin):
-    list_display = ('name', 'inn', 'ter_administration', 'address')
+    list_display = ('name', 'inn', 'ter_administration', 'address', 'get_students_count')
     search_fields = ('name', 'inn', 'address__city__name')
     list_filter = (
         ('ter_administration', RelatedOnlyDropdownFilter), 
     )
     inlines = [GradeInLine,]
+
+    def get_students_count(self, school):
+        students_count = len(school.students.all())
+        return students_count
+    get_students_count.short_description='Колво учеников'
+
     
 @admin.register(Grade)
 class SchoolClassAdmin(admin.ModelAdmin):
@@ -37,4 +43,38 @@ class SchoolContactPersoneAdmin(admin.ModelAdmin):
 
 @admin.register(SchoolStudent)
 class SchoolStudentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('get_last_name', 'get_first_name', 'get_middle_name', 'get_email', 'school', 'get_grade')
+    list_filter = (
+        ('school', RelatedOnlyDropdownFilter),
+        ('grade', RelatedOnlyDropdownFilter),
+    )
+
+    def get_last_name(self, student):
+        last_name = student.user.last_name
+        return last_name
+    get_last_name.short_description='Фамилия'
+    get_last_name.admin_order_field = 'student__user__last_name'
+
+    def get_first_name(self, student):
+        first_name = student.user.first_name
+        return first_name
+    get_first_name.short_description='Имя'
+    get_first_name.admin_order_field = 'student__user__first_name'
+
+    def get_middle_name(self, student):
+        middle_name = student.user.middle_name
+        return middle_name
+    get_middle_name.short_description='Отчество'
+    get_middle_name.admin_order_field = 'student__user__middle_name'
+
+    def get_email(self, student):
+        email = student.user.email
+        return email
+    get_email.short_description='Email'
+    get_email.admin_order_field = 'student__user__email'
+
+    def get_grade(self, student):
+        classes = f'{student.grade.grade}{student.grade.grade_letter}'
+        return classes
+    get_grade.short_description='Класс'
+    get_grade.admin_order_field = 'student__grade'
